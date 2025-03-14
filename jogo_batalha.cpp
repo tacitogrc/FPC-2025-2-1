7f5a9fb5828a1c46d1ea1d9ac96a36a4d7818579
Tácito Henrique <tacitogrc@gmail.com>
2025-03-14 20:14:09 -0300

    lógica de batalhas

--- jogo_batalha.cpp
+++ jogo_batalha.cpp
@@ -3,58 +3,75 @@
 #include <cmath>
 #include <ctime>
 
-class FormaDeVida {
+using namespace std;
+
+class FormaDeVida
+{
 protected:
     string nome;
     int vida;
     int forca;
 
 public:
-    FormaDeVida() {
-			vida = 100;
-			forca = 100;
-		}
-		FormaDeVida(string n) {
-			nome = n;
-			vida = 100;
-			forca = 100;
+    FormaDeVida()
+    {
+        vida = 100;
+        forca = 100;
     }
-    
+    FormaDeVida(string n)
+    {
+        nome = n;
+        vida = 100;
+        forca = 100;
+    }
+
     bool estaVivo() { return vida > 0; }
     string getNome() { return nome; }
     void setNome(string n) { nome = n; }
     int getVida() { return vida; }
-    virtual void atacar(FormaDeVida &alvo) {
+    virtual void atacar(FormaDeVida &alvo)
+    {
         int dano = (rand() % forca) + 1;
         alvo.receberDano(dano);
         cout << nome << " ataca " << alvo.getNome() << " causando " << dano << " de dano!\n";
     }
-    void receberDano(int dano) {
+    void receberDano(int dano)
+    {
         vida = vida - dano;
-        if (vida < 0) {vida = 0};
+        if (vida < 0)
+        {
+            vida = 0;
+        }
     }
 };
 
-int main(void) {
+int main(void)
+{
+    srand(time(0)); // Initialize random seed
 
-  cout << "Sherek enfrenta Granamir!\n";
+    cout << "Sherek enfrenta Granamir!\n";
 
-  FormaDeVida sherek, granamir;
+    FormaDeVida sherek, granamir;
 
-  sherek.setNome("Sherek");
-  granamir.setNome("Granamir");
-    
-  while (sherek.estaVivo() && granamir.estaVivo()) {
-    shereik.atacar(granamir);
-    if (granamir.estaVivo()) granamir.atacar(shereik);
-  }
-    
-  if (sherek.estaVivo()) {
-    cout << "Shereik venceu e absorveu o poder do dragão!\n";
-    } else {
-    cout << "Granamir venceu! O reino está condenado...\n";
-    return 0;
+    sherek.setNome("Sherek");
+    granamir.setNome("Granamir");
+
+    while (sherek.estaVivo() && granamir.estaVivo())
+    {
+        sherek.atacar(granamir);
+        if (granamir.estaVivo())
+            granamir.atacar(sherek);
     }
-	
-return 0;
+
+    if (sherek.estaVivo())
+    {
+        cout << "Sherek venceu e absorveu o poder do dragão!\n";
+    }
+    else
+    {
+        cout << "Granamir venceu! O reino está condenado...\n";
+        return 0;
+    }
+
+    return 0;
 }
